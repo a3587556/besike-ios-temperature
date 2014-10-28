391fda5d467d3b5eb550487e3d867dd5298e225d
a3587556 <315803679@qq.com>
2014-10-28 09:35:36 +0800

    Initial Commit

---
+++ temperature/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  temperature
+//
+//  Created by darkgod on 14/10/28.
+//  Copyright (c) 2014年 darkgod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
